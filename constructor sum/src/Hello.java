754745308eaca769001ba694a54f11efb456fbaa
vyshnav <vyshnav@rog>
2022-04-07 21:46:45 +0530

    first commit

---
+++ constructor sum/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+	public static void main(String arg[]) {
+		Sum obj1=new Sum();
+		Sum obj2=new Sum();
+		obj1.a=10;
+		obj1.b=5;
+		obj2.a=20;
+		obj2.b=25;
+		obj1.ASum();
+		obj2.ASum();
+		obj1.display();
+		obj2.display();
+		
+		
+	}
+
+}
---
+++ constructor sum/src/Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+	
+int a;
+int b;
+int Sum;
+void ASum(){
+Sum=a+b;
+}
+void display() {
+	System.out.println("sum is "+Sum);
+}
+
+}
